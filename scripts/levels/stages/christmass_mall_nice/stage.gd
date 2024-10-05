164fb4bcc4dbcd991a58af8f3e9504635987bb42
GiNUser <fnmoutmail014@gmail.com>
2024-10-05 22:30:06 +0300

    Delete scripts directory

--- scripts/levels/stages/christmass_mall_nice/stage.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready():
-	$anim_player.speed_scale = get_tree().root.get_node("level").bps
--- scripts/levels/stages/school/freak.gd
+++
@@ -1,6 +0,0 @@
-extends AnimatedSprite2D
-
-@export var start_anim = "angry0"
-
-func _ready():
-	play(start_anim)
